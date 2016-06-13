b5c226ca453eae535561254dedec3c3fafb1792d
bharathi <bharathi@invoscape.com>
2016-06-13 18:11:49 +0530

    Added JS for dropdown button of sign_up page .

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,13 @@
+$( document ).on('ready page:load', function() {
+  $('.select_country').click(function () {
+    $('.add_class_business').removeClass("dropdown_business");
+    $('.add_class_country').addClass("dropdown_country");
+  });
+  $('.select_business').click(function () {
+    $('.add_class_business').addClass("dropdown_business");
+  });
+  $('.select_state').click(function () {
+    $('.add_class_country').removeClass("dropdown_country");
+    $('.add_class_business').removeClass("dropdown_business");
+  });
+});
