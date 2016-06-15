67317e3ed94df2fb1474bca27adc871e803a3ac1
bharathi <bharathi@invoscape.com>
2016-06-15 19:49:06 +0530

    create dropdown_list.rb file ,avoiding code complexity

--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -1,4 +1,6 @@
 class CompanyProfile < ActiveRecord::Base
+  include DropdownList
+
   has_many :office_addresses
   has_many :authorized_signatories
   validates :company_name, :email, :company_website, :company_type, :pan_no, :corp_id_no, presence: true
@@ -6,9 +8,9 @@ class CompanyProfile < ActiveRecord::Base
   accepts_nested_attributes_for :office_addresses
   accepts_nested_attributes_for :authorized_signatories
 
-  COMPANY = ["private", "private"]
-  POSITION = ["Managing Director", "Managing Director"]
-  STATE = ["Tamil Nadu", "Tamil Nadu"]
+  COMPANY = LIST_OF_COMPANY.zip(LIST_OF_COMPANY)
+
+  POSITION = POSITION_LIST.zip(POSITION_LIST)
 
 end
 
