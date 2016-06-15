82e4cae34c9bc8fa9f47bee718256579450c622e
bharathi <bharathi@invoscape.com>
2016-06-15 16:30:06 +0530

    Added model validate for company_profile

--- app/models/authorized_signatory.rb
+++ app/models/authorized_signatory.rb
@@ -1,3 +1,4 @@
 class AuthorizedSignatory < ActiveRecord::Base
   belongs_to :company_profile
+  validates :person_name, :person_position, :person_id_no, presence: true
 end
--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -1,6 +1,7 @@
 class CompanyProfile < ActiveRecord::Base
   has_many :office_addresses
   has_many :authorized_signatories
+  validates :company_name, :email, :company_website, :company_type, :pan_no, :corp_id_no, presence: true
 
   accepts_nested_attributes_for :office_addresses
   accepts_nested_attributes_for :authorized_signatories
